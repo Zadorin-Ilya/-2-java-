010c3cc12884d04e6bfa52c2b76d56f16c5923db
Илья Задорин <zadorinilja@yandex.ru>
2022-07-26 20:27:02 +0400

    Main one

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,18 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int i = 100; // Начальный баланс лицевого счета
+        int balans = 100; // Начальный баланс лицевого счета
         int sum = 100; // Сумма от которой начисляется бонус
-        int x = 1200; // Сумма пополнения клиентом
+        int deposit = 1200; // Сумма пополнения клиентом
 
         int bonus;
-        if (x > 1000) {
-            bonus = x / sum;
+        if (deposit > 1000) {
+            bonus = deposit / sum;
         } else {
             bonus = 0;
         }
 
-        int finish = x + i + bonus;
+        int finish = deposit + balans + bonus;
 
 
         System.out.println("Итого сумма = " + finish);
